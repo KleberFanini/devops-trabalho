0268399f9df549fe2d7f9857b3b0f7870537a8c6
Kleber Fanini <kleberdev97@gmail.com>
2025-12-31 13:28:38 +0000

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const PORT = 80;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log('Servidor rodando na porta 80');
+});
